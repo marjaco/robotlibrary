a6a1151cc3d8e312c699cfdd5c99c8fa9136af24
Marcus Jacobs <west.wallaby@posteo.de>
2024-06-21 16:30:04 +0200

    Class joystick refined. Direction is given as 0 to 90 or 0 to -90, speed is given as 0 to 100 or 0 to -100.

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 This is a library written in micropython for a modified SMARS robot, driven by a Raspberry Pi Pico. The follwing sensors / parts can be used with this: 
 + ultrasonic sensor
 + infrared sensor
-+ servo motor that turns the ujltrasonic sensor for sweeps of the surroundings.
++ servo motor that turns the ultrasonic sensor for sweeps of the surroundings.
   
+You have also two classes for different types of remote controls. (work in progress)
--- joystick.py
+++ joystick.py
@@ -1,40 +1,75 @@
-from machine import Pin,ADC
+from machine import Pin,ADC,Timer
 import utime
 from robotlibrary.motor import Motor
+X_MEDIAN = 29940
+Y_MEDIAN = 30510
+MAX_DUTY = 65535
+MIN_DUTY = 260
+DEBOUNCE_WAIT = 100
 class Joystick:
     def __init__(self, x,y,b):
         self.x = ADC(x)
         self.y = ADC(y)
         self.b = Pin(b,Pin.IN, Pin.PULL_UP)
         self.b.irq(trigger=Pin.IRQ_FALLING, handler=self.button_handler)
+        self.pressed = False
+        self.last_pressed = 0
+        self.timer = Timer()
+        
+    def reset(self,t):
+        self.pressed = False
     
     def button_handler(self,pin):
-        pass
+        while utime.ticks_diff(utime.ticks_ms(), self.last_pressed) < DEBOUNCE_WAIT: 
+            pass
+        self.last_pressed = utime.ticks_ms()
+        if not self.pressed:
+            while utime.ticks_diff(utime.ticks_ms(), self.last_pressed) < DEBOUNCE_WAIT:
+                pass
+            if pin.value() == 1:
+                self.pressed=True 
+                print(pin.value(), "Button pressed")
+                self.last_pressed = utime.ticks_ms()
+                self.timer.init(mode=Timer.ONE_SHOT, period=300, callback=self.reset)
     
     def get_speed(self,s):
         speed = 0
-        if s < 29600:
-            speed = abs(Motor.MAX_DUTY-s*2)
-        elif s > 30500:
-            speed = -abs(Motor.MAX_DUTY-s*2)    
+        if s < Y_MEDIAN-300:
+            speed = abs(MAX_DUTY-s*2)
+        elif s > Y_MEDIAN+300:
+            speed = -abs(MAX_DUTY-s*2)    
         else:
             speed = 0
         if speed > -4000 and speed < 4000:
                 speed = 0
-        return int(100/Motor.MAX_DUTY*speed)
-    
+        return int(100/MAX_DUTY*speed)
     
     def get_direction(self,d):
-        if s < 29600:
-            return abs(65535-s*2)
-        elif s > 30500:
-            return -abs(65535-s*2)
+        direction = 0
+        if d < X_MEDIAN-200:
+            direction = -abs(90-(90/X_MEDIAN*d))
+        elif d > X_MEDIAN+200:
+            direction = abs(90/65535*d)
         else:
-            return 0
+            direction = 0
+        return int(direction)
 
 joystick = Joystick(26,27,0)
 
 while True:
+#     y_values = list()
+#     x_values = list()
+#     for i in range(0,100):
+#         x_values.append(joystick.x.read_u16())
+#         y_values.append(joystick.y.read_u16())
+#     sum = 0
+#     print(y_values)
+#     for y in y_values:
+#         sum += y
+    #print(f"Yvalues median: {sum/100}")
     #print(f"X-Werte: {joystick.x.read_u16()}")
+    #print(f"Y-Werte: {joystick.y.read_u16()}")
+
+    print(f"X-Werte: {joystick.get_direction(joystick.x.read_u16())}")
     print(f"Y-Werte: {joystick.get_speed(joystick.y.read_u16())}")
     utime.sleep_ms(1000)
