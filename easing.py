9b67783a312d6b337cf8767ca61fb1a8db51798f
Marcus Jacobs <west.wallaby@posteo.de>
2025-05-07 15:10:47 +0200

    Added a provisional file for servo easing.

--- easing.py
+++ easing.py
@@ -1,32 +1,23 @@
 import math
 from robotlibrary.servo import Servo
 from time import sleep_ms
-def loop(start, stop, angle):
-    increment = 2/angle
-    i= start
-    values = list()
-    sum = 0
+
+def get_steps(start, stop, d_angle):
+    increment = stop/d_angle
+    i = start
+    steps = list()
+    sum_steps = 0
     while i < stop-increment:
-        sum = sum+ ease_in_out_sine(i)
+        sum_steps = sum_steps + ease_in_out_sine(i)
         #print(f"i: {i}, Ease: {ease_in_out_sine(i)}")
         i=i+increment
-    #print(sum)
-    i=start
+    i = start
     while i < stop-increment:
-        #sum = sum+ ease_in_out_quad(i)
+        #sum_steps = sum_steps+ ease_in_out_quad(i)
         #print(f"i: {i}, Ease: {ease_in_out_sine(i)}")
-        values.append(ease_in_out_sine(i)*angle/sum)
+        steps.append(ease_in_out_sine(i)*d_angle/sum_steps)
         i=i+increment
-    sumf = 0.00
-    for v in values:
-        #print(v)
-        sumf = sumf+v
-    
-    
-    #print(sumf)
-    #print(sum(values))
-    return values
-    
+    return steps
 
 def ease_in_out_quad(t: float) -> float:
         t *= 2
@@ -38,37 +29,38 @@ def ease_in_out_quad(t: float) -> float:
 def ease_in_out_sine(t: float) -> float:
         return -0.5 * (math.cos(math.pi * t) - 1)
 
-
 #print(loop(0,2,15))
 servo = Servo(0,True,1350,8100)
 servo2 = Servo(1,True,1350,8100)
-angles = loop(0,2,90)
+angles = get_steps(0,2,90)
 
 while True: 
     for i in range(0,5):
-        tmp=60
+        tmp=60 # start angle
         for a in angles:
             tmp = tmp+a
-            #print(tmp)
+            print(tmp)
             servo.set_angle(tmp)
             servo2.set_angle(tmp)
             sleep_ms(2)
         for a in angles:
             tmp = tmp-a
-            #print(tmp)
+            print(tmp)
             servo.set_angle(tmp)
             servo2.set_angle(tmp)
             sleep_ms(2)
         
     for i in range(0,5):
-        tmp=60
+        tmp=60 # start angle
         for a in range(0,15):
             tmp = tmp + a
+            print(tmp)
             servo.set_angle(tmp)
             servo2.set_angle(tmp)
             sleep_ms(20)
         for a in range(15,0,-1):
             tmp = tmp-a
+            print(tmp)
             servo.set_angle(a)
             servo2.set_angle(tmp)
             sleep_ms(20)
