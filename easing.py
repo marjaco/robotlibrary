d31915a47e31d1849eba9f628f90bc654ef38f68
Marcus Jacobs <west.wallaby@posteo.de>
2025-05-23 15:07:00 +0200

    minor changes (internal) in easing.py.

--- easing.py
+++ easing.py
@@ -4,7 +4,10 @@ from time import sleep_ms
 from collections import deque
 
 def get_steps(start, stop, d_angle):
-    '''Calculates the necessary steps for the given angle d_angle to make a smooth movmeent'''
+    '''Calculates the necessary steps for the given angle d_angle to make a smooth movement.
+    :param start: 0
+    :param stop: 0
+    :d_angle: The angle the sensor shall move. '''
     #print(d_angle)
     increment = stop/d_angle #This is one way to determine the increment. Others are better.
     #The bigger the angle, the smaller the increment is noch a good idea.
@@ -22,18 +25,18 @@ def get_steps(start, stop, d_angle):
     while i < stop-increment:
         #sum_steps = sum_steps+ ease_in_out_sine(i)
         #print(f"i: {i}, Ease: {ease_in_out_sine(i)}")
-        steps.append(ease_in_out_sine(i)*d_angle/sum_steps)
+        steps.append(_ease_in_out_sine(i)*d_angle/sum_steps)
         i=i+increment
     return deque(steps,len(steps))
 
-def ease_in_out_quad(t: float) -> float:
+def _ease_in_out_quad(t: float) -> float:
         t *= 2
         if t < 1:
             return 0.5 * t * t
         t -= 1
         return -0.5 * (t * (t - 2) - 1)
     
-def ease_in_out_sine(t: float) -> float:
+def _ease_in_out_sine(t: float) -> float:
         return -0.5 * (math.cos(math.pi * t) - 1)
 
 
