f9ae48d5564a1bc43060c122118d050beccd43b2
marjaco <west.wallaby@posteo.de>
2026-03-24 09:03:46 +0100

    double joystick

---
+++ double_joystick.py
@@ -0,0 +1,125 @@
+# Version 2.0.2
+from machine import Pin,ADC,Timer
+from robotlibrary import config as conf
+from time import sleep, sleep_ms,ticks_diff, ticks_ms
+
+from robotlibrary.motor import Motor
+from collections import deque
+
+class Double_Joystick:
+    def __init__(self, x,y,z,b1,b2):
+        self.x = ADC(x)
+        self.y = ADC(y)
+        self.z = ADC(z)
+        self.b1 = Pin(b,Pin.IN, Pin.PULL_UP)
+        self.b1.irq(trigger=Pin.IRQ_RISING, handler=self.button_handler)
+        self.b2 = Pin(b,Pin.IN, Pin.PULL_UP)
+        self.b2.irq(trigger=Pin.IRQ_RISING, handler=self.button_handler)
+        self.pressed = False
+        self.last_pressed = 0
+        self.timer = Timer()
+        self.direction_data = deque([conf.JS_X_MEDIAN,conf.JS_X_MEDIAN,conf.JS_X_MEDIAN,conf.JS_X_MEDIAN,conf.JS_X_MEDIAN],5)
+        self.speed_data = deque([conf.JS_Y_MEDIAN,conf.JS_Y_MEDIAN,conf.JS_Y_MEDIAN,conf.JS_Y_MEDIAN,conf.JS_Y_MEDIAN],5)
+        self.z_data = deque([conf.JS_Y_MEDIAN,conf.JS_Y_MEDIAN,conf.JS_Y_MEDIAN,conf.JS_Y_MEDIAN,conf.JS_Y_MEDIAN],5)
+        
+    def get_button_pressed(self):
+        return self.pressed
+    
+    def reset(self,t):
+        self.pressed = False
+    
+    def button_handler(self,pin):
+        while ticks_diff(ticks_ms(), self.last_pressed) < conf.DEBOUNCE_WAIT: 
+            pass
+        self.last_pressed = ticks_ms()
+        if not self.pressed:
+            while ticks_diff(ticks_ms(), self.last_pressed) < conf.DEBOUNCE_WAIT:
+                pass
+            if pin.value() == 1:
+                self.pressed=True 
+                print(pin.value(), "Button pressed")
+                self.last_pressed = ticks_ms()
+                self.timer.init(mode=Timer.ONE_SHOT, period=300, callback=self.reset)
+    
+    def get_speed(self):
+        s = self.y.read_u16()
+        self.speed_data.append(s)
+        s = int(sum(self.speed_data)/len(self.speed_data))
+        speed = 0
+        if s < conf.JS_Y_MEDIAN-300:
+            speed = abs(conf.JS_MAX_DUTY-s*2)
+        elif s > conf.JS_Y_MEDIAN+300:
+            speed = -abs(conf.JS_MAX_DUTY-s*2)    
+        else:
+            speed = 0
+        if speed > -1000 and speed < 1000:
+                speed = 0
+        return int(conf.MAX_SPEED/conf.JS_MAX_DUTY*speed) * conf.JS_REVERSED
+    
+    def get_x(self):
+        '''This does the same as get_direction(). You might use this if you want
+        to use the joystick in another way. It is easier to read the code then. '''
+        return get_speed()
+    
+    def get_y(self):
+        '''This does the same as get_direction(). You might use this if you want
+        to use the joystick in another way. It is easier to read the code then. ''' 
+        return get_direction()
+    
+    def get_z(self):
+        '''This is the y -axis of the secons joystick.'''
+        s = self.z.read_u16()
+        self.z_data.append(s)
+        s = int(sum(self.z_data)/len(self.z_data))
+        z = 0
+        if s < conf.JS_Y_MEDIAN-300:
+            z = abs(conf.JS_MAX_DUTY-s*2)
+        elif s > conf.JS_Y_MEDIAN+300:
+            z = -abs(conf.JS_MAX_DUTY-s*2)    
+        else:
+            z = 0
+        if z > -1000 and z < 1000:
+                z = 0
+        return int(conf.MAX_SPEED/conf.JS_MAX_DUTY*z) * conf.JS_REVERSED
+
+    def get_direction(self):
+        d = self.x.read_u16()
+        self.direction_data.append(d)
+        d = int(sum(self.direction_data)/len(self.direction_data))
+        direction = 0
+        if d < conf.JS_X_MEDIAN-200:
+            #direction = - abs(90-(90/conf.JS_MAX_DUTY * d))
+            direction = abs(conf.JS_MAX_DUTY-d*2)*-1
+        elif d > conf.JS_X_MEDIAN+200:
+            #direction = abs(90/conf.JS_MAX_DUTY * d)
+            direction = abs(conf.JS_MAX_DUTY-d*2)
+        else:
+            direction = 0
+        return int(90/conf.JS_MAX_DUTY*direction) * conf.JS_REVERSED
+
+    def calibration(self):
+        print("calibrating, don't move the joystick.", end='')
+        x = [conf.JS_X_MEDIAN for a in range(1000)]
+        y = [conf.JS_Y_MEDIAN for b in range(1000)]
+        for i in range(0,len(x)):
+            print(".", end="")
+            x[i] = self.x.read_u16()
+            y[i] = self.y.read_u16()
+            sleep_ms(2)
+        print(" ")
+        dx = int(sum(x)/len(x))
+        dy = int(sum(y)/len(y))
+        print(f"JS_X_MEDIAN value (direction): {dx}")
+        print(f"JS_Y_MEDIAN value (speed): {dy}")
+        print("Use those values in config.py.")
+
+def main():
+    joystick = Double_Joystick(26,27,28,0,1)
+    joystick.calibration()
+#     while True:
+#         print(f"Speed: {joystick.get_speed()}, Direction: {joystick.get_direction()}")
+#         sleep_ms(50)
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
---
+++ rc_double_joystick.py
@@ -0,0 +1,77 @@
+# Version 2.0.2
+########## Import the configuration
+from robotlibrary import config as conf
+from robotlibrary.rotary import Rotary
+from robotlibrary.joystick import Joystick
+
+########## Import bluetooth library
+from robotlibrary.bluetooth.central import BLECentral
+from robotlibrary.bluetooth.ble_services_definitions import MOTOR_TX_UUID, MOTOR_RX_UUID, ROBOT_UUID
+from robotlibrary.bluetooth.parser import encode_motor, decode_motor
+
+########## Import pico micropython libraries
+from time import sleep, sleep_ms
+from machine import Timer,ADC
+import micropython
+micropython.alloc_emergency_exception_buf(100)
+
+class RC:
+    '''This class represents the remote control with two rotary encoders and a slider to set the speed. Don't edit unless you know what you are doing. '''
+    def __init__(self):
+        self.forward = True
+        self.speed = 0
+        self.z = 0
+        self.turn_val = 0 # 0=straight on; >0=turn right; <0=turn left
+        self.button_pressed = False
+        #self.change = True
+        self.joystick = Double_Joystick(conf.X_PIN,conf.Y_PIN,28,conf.B_PIN,1) # Pins for x axis, y axis and button
+        self.timer = Timer()
+        self.timer.init(mode=Timer.PERIODIC, period=50, callback=self.set_values)
+        self.send_timer = Timer()
+        self.send_timer.init(mode=Timer.PERIODIC, period=200, callback=self.send)
+        self.duty_cycle = 0
+        self.server = BLECentral(conf.ROBOT_NAME, True)
+        self.server.register_read_callback(MOTOR_TX_UUID, self.read)
+        self.server.scan()
+        self.button_pressed_sent = False
+        print("waiting for connection")
+        while not self.server.is_connected():
+            sleep(1)
+        sleep(5)
+        print("Found connection")
+            
+    def read(self,a):
+        print("read")
+    
+    def get_button_pressed(self):
+        if self.button_pressed_sent == False and self.button_pressed:
+            return True
+        sleep_ms(20)
+        self.button_pressed_sent = False
+        return False
+    
+    def send(self,t): 
+        print(f"sending data ... speed: {self.speed}, turn: {self.turn_val}, forward: {self.forward}, button: {self.get_button_pressed()}")
+        data = encode_motor(self.speed, self.turn_val, self.forward, self.get_button_pressed(), self.z)
+        self.server.send(ROBOT_UUID, MOTOR_RX_UUID, data)
+        if self.button_pressed:
+            self.button_pressed_sent = True
+                
+    def set_values(self,t): 
+        '''This calculates the speed between MIN_SPEED and MAX_SPEED that is sent to the robot.'''
+        s = self.joystick.get_speed()
+        self.speed = abs(s)
+        self.turn_val = self.joystick.get_direction()
+        self.forward = s >= 0
+        self.button_pressed = self.joystick.pressed
+        self.z = self.joystick.get_z()
+        
+def main():
+    rc = RC()
+    while True:
+        sleep_ms(500)
+    
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
+
